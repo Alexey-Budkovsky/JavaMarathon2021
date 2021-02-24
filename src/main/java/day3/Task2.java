f4439f76a292b6c9b11d5c499c41d560a7563395
Alexey <budkovsky.alex@gmail.com>
2021-02-25 00:37:27 +0300

    day3: add Task1-Task3 solutions

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.println("Введите число для делимого");
+            double dividend = input.nextDouble();
+            System.out.println("Введите число для делителя");
+            double divider = input.nextDouble();
+            if (divider != 0) {
+                System.out.println(dividend / divider);
+            } else {
+                break;
+            }
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        for(int i =0; i < 5; i++) {
+            System.out.println("Введите число для делимого");
+            double dividend = input.nextDouble();
+            System.out.println("Введите число для делителя");
+            double divider = input.nextDouble();
+            if (divider == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(dividend / divider);
+        }
     }
 }
